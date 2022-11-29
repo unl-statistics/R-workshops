02154cce9980c406aa7c8f37e7f0552cb746a810
Susan Vanderplas <srvanderplas@gmail.com>
2022-11-29 12:12:19 -0600

    Update main page files

--- create_R_files.R
+++ create_R_files.R
@@ -1,13 +1,23 @@
 library(tidyverse)
 
+# copy README to index
+file.copy("README.qmd", "index.qmd", overwrite = T)
+
 # List all slide Rmd files
-input <- list.files(pattern = "*.Rmd", full.names = T, recursive = T)
-input <- input[str_detect(input, "knitr")]
+input <- list.files(pattern = "*.(q|R)md", full.names = T, recursive = T)
+input <- input[str_detect(input, "knitr|index|README")]
 input <- input[!str_detect(input, "Old|summerschool")]
 
+
+
 # Location to put the code files
-output <- str_replace(input, "/knitr/", "/code/") %>%
-  str_replace("\\.Rmd", ".R")
+r_output <- str_replace(input, "/knitr/", "/code/") %>%
+  str_replace("\\.(R|q)md", ".R")
+
+html_output <- str_replace(input, "(q|R)md$", "html")
+
+# Run through all the markdown files and compile
+purrr::walk(input, ~rmarkdown::render(.))
 
 # Run through everything with purl
-purrr::map2(input, output, knitr::purl)
+purrr::map2(input, r_output, knitr::purl)
